11b10d4332f69722c7a892625d2b914715aba636
akshat20000 <akshatprashar2710@gmail.com>
2025-10-26 19:36:20 +0530

    day 2

--- build/CMakeFiles/stock_engine.dir/DependInfo.cmake
+++ build/CMakeFiles/stock_engine.dir/DependInfo.cmake
@@ -8,10 +8,11 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/mnt/e/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/database.cpp" "CMakeFiles/stock_engine.dir/src/database.cpp.o" "gcc" "CMakeFiles/stock_engine.dir/src/database.cpp.o.d"
-  "/mnt/e/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/main.cpp" "CMakeFiles/stock_engine.dir/src/main.cpp.o" "gcc" "CMakeFiles/stock_engine.dir/src/main.cpp.o.d"
-  "/mnt/e/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/matching_engine.cpp" "CMakeFiles/stock_engine.dir/src/matching_engine.cpp.o" "gcc" "CMakeFiles/stock_engine.dir/src/matching_engine.cpp.o.d"
-  "/mnt/e/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/orderbook.cpp" "CMakeFiles/stock_engine.dir/src/orderbook.cpp.o" "gcc" "CMakeFiles/stock_engine.dir/src/orderbook.cpp.o.d"
+  "E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/database.cpp" "CMakeFiles/stock_engine.dir/src/database.cpp.obj" "gcc" "CMakeFiles/stock_engine.dir/src/database.cpp.obj.d"
+  "E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/main.cpp" "CMakeFiles/stock_engine.dir/src/main.cpp.obj" "gcc" "CMakeFiles/stock_engine.dir/src/main.cpp.obj.d"
+  "E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/matching_engine.cpp" "CMakeFiles/stock_engine.dir/src/matching_engine.cpp.obj" "gcc" "CMakeFiles/stock_engine.dir/src/matching_engine.cpp.obj.d"
+  "E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/orderbook.cpp" "CMakeFiles/stock_engine.dir/src/orderbook.cpp.obj" "gcc" "CMakeFiles/stock_engine.dir/src/orderbook.cpp.obj.d"
+  "E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend/src/redis_client.cpp" "CMakeFiles/stock_engine.dir/src/redis_client.cpp.obj" "gcc" "CMakeFiles/stock_engine.dir/src/redis_client.cpp.obj.d"
   )
 
 # Targets to which this target links which contain Fortran sources.
--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: /mnt/e/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend
+# Install script for directory: E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/backend
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "/usr/local")
+  set(CMAKE_INSTALL_PREFIX "C:/Program Files (x86)/Stock-Exchange-Engine")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -27,11 +27,6 @@ if(NOT CMAKE_INSTALL_COMPONENT)
   endif()
 endif()
 
-# Install shared libraries without execute permission?
-if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
-  set(CMAKE_INSTALL_SO_NO_EXE "1")
-endif()
-
 # Is this installation the result of a crosscompile?
 if(NOT DEFINED CMAKE_CROSSCOMPILING)
   set(CMAKE_CROSSCOMPILING "FALSE")
@@ -39,7 +34,7 @@ endif()
 
 # Set default install directory permissions.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "/usr/bin/objdump")
+  set(CMAKE_OBJDUMP "C:/mingw64/bin/llvm-objdump.exe")
 endif()
 
 if(CMAKE_INSTALL_COMPONENT)
@@ -50,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/mnt/e/My_personal/Projects/ongoing/Stock_Exchange_Engine/build/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "E:/My_personal/Projects/ongoing/Stock_Exchange_Engine/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
